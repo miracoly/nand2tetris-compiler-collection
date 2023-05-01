2060d263f4290b230987c31170395a9592a24f4a
miracoly <68049792+miracoly@users.noreply.github.com>
2023-05-01 17:55:31 +0200

    complete cleanUpCode function and add instruction data type

--- src/Hack/Assembler/Internal.hs
+++ src/Hack/Assembler/Internal.hs
@@ -2,9 +2,34 @@
 
 module Hack.Assembler.Internal (module Hack.Assembler.Internal) where
 
+import Control.Applicative (ZipList (..), liftA2)
 import Data.Maybe (fromMaybe)
-import Data.Text as T (Text, filter, lines, stripSuffix, unlines, take, strip)
-import Prelude as P hiding (dropWhile, lines, unlines, length, take)
+import Data.Text as T (Text, elem, filter, lines, strip, stripSuffix, take, unlines)
+import Numeric (showBin)
+import Prelude as P hiding (dropWhile, elem, length, lines, take, unlines)
+
+-- | Represent CPU Instruction of Hack Computer.
+--
+-- Read the complete [specification of the Hack instructions](https://www.nand2tetris.org/_files/ugd/44046b_89a8e226476741a3b7c5204575b8a0b2.pdf) for in-depth explanation.
+data Instruction
+  = -- | A instruction with 15 Bit value.
+    A' Int
+  | -- | C instruction consisting of 13 Bit command.
+    C'
+      { -- | 1 Bit value. When 0 computation is done with value of A Register, otherwise with memory value A register points to.
+        _a :: Int,
+        -- | 6 Bit computation instruction of the ALU
+        _comp :: Int,
+        -- | 3 Bit value which defines the destination where the result of the ALU computation is written to.
+        _dest :: Int,
+        -- | 3 Bit value which defines when to jump.
+        _jump :: Int
+      }
+
+instance Show Instruction where
+  show i = case i of
+    A' n -> "A " <> showBin n ""
+    C' a c d j -> "C " <> unwords (getZipList $ liftA2 showBin (ZipList [a, c, d, j]) $ ZipList $ repeat "")
 
 -- | Remove whitespaces, empty lines and comments.
 cleanUpCode :: Text -> Text
@@ -12,11 +37,13 @@ cleanUpCode =
   fromMaybe ""
     . stripSuffix "\n"
     . unlines
-    . P.filter (/= "")
-    . P.filter (/= "")
-    . P.filter (/= "\n")
+    . P.filter isCode
     . T.lines
     . T.filter (/= ' ')
+
+-- | Return True if Text does NOT contain any white spaces, new lines or comments, otherwise False.
+isCode :: Text -> Bool
+isCode = and . fmap not . sequenceA [isComment, (== ""), (== "\n"), elem ' ', elem '\n']
 
 -- | Return True if Text is comment, otherwise False.
 isComment :: Text -> Bool
--- test/Hack/AssemblerSpec.hs
+++ test/Hack/AssemblerSpec.hs
@@ -2,12 +2,22 @@
 
 module Hack.AssemblerSpec (spec) where
 
-import Hack.Assembler.Internal (cleanUpCode, isComment)
+import Data.Text (pack)
+import Hack.Assembler.Internal (cleanUpCode, isCode, isComment, Instruction(..))
 import Test.Hspec
+import Test.QuickCheck (elements, forAll, listOf1, property)
 import Test.QuickCheck.Instances.Text ()
 
 spec :: Spec
 spec = do
+  describe "Binary is member of Read class" $ do
+    it "shows A Instruction" $ do
+      show (A' 12) `shouldBe` "A 1100"
+      show (A' 100) `shouldBe` "A 1100100"
+    it "shows C Instruction" $ do
+      show (C' 1 8 6 7) `shouldBe` "C 1 1000 110 111"
+      show (C' 0 60 4 5) `shouldBe` "C 0 111100 100 101"
+
   describe "cleanUpCode" $ do
     it "can handle empty text" $ do
       cleanUpCode "" `shouldBe` ""
@@ -18,6 +28,22 @@ spec = do
       cleanUpCode "a \n b b" `shouldBe` "a\nbb"
     it "can handle multiple new lines" $ do
       cleanUpCode "a \n\n b\n \n b" `shouldBe` "a\nb\nb"
+    it "can remove comments" $ do
+      cleanUpCode "a\n// i am a comment" `shouldBe` "a"
+      cleanUpCode "a\n// i am a comment\nabc" `shouldBe` "a\nabc"
+      cleanUpCode "a \n // i am a comment \n abc " `shouldBe` "a\nabc"
+
+  describe "isCode" $ do
+    it "detects code" $
+      property $
+        let validElements = elements $ ['a' .. 'z'] <> ['0' .. '9']
+            genValidText = pack <$> listOf1 validElements
+         in forAll genValidText $ \t -> isCode t `shouldBe` True
+    it "detects empty text" $ do
+      isCode "" `shouldBe` False
+    it "detects white spaces" $ do
+      isCode " a" `shouldBe` False
+      isCode "a " `shouldBe` False
 
   describe "isComment" $ do
     it "can handle empty text" $ do
