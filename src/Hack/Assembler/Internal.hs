424229f8f049c5ac4c22abf58d1f21be884f9d07
miracoly <68049792+miracoly@users.noreply.github.com>
2023-05-02 18:27:24 +0200

    add splitting of instruction

--- src/Hack/Assembler/Internal.hs
+++ src/Hack/Assembler/Internal.hs
@@ -2,12 +2,14 @@
 
 module Hack.Assembler.Internal (module Hack.Assembler.Internal) where
 
-import Control.Applicative (ZipList (..), liftA2)
+import Control.Applicative (ZipList (..), liftA2, (<|>))
+import Control.Monad (join)
+import Data.List as L (head, length)
 import Data.Maybe (fromMaybe)
-import Data.Text as T (Text, elem, filter, head, length, lines, null, strip, stripSuffix, tail, take, unlines, unpack)
+import Data.Text as T (Text, elem, filter, head, length, lines, null, split, strip, stripSuffix, tail, take, unlines, unpack)
 import Data.Text.Read (Reader, decimal)
 import Numeric (showBin)
-import Prelude as P hiding (dropWhile, length, lines, null, take, unlines)
+import Prelude as P hiding (dropWhile, length, lines, null, span, take, unlines)
 
 -- | Represent CPU Instruction of Hack Computer.
 --
@@ -39,8 +41,8 @@ type ParseErrorDescription = String
 -- otherwise 'Data.Either.Left' 'ParseErrorDescription'.
 parseInstruction :: Text -> Either ParseErrorDescription Instruction
 parseInstruction instr
-  | null instr = Left $ "Could not parse " <> unpack instr
-  | length instr < 2 = parseC instr
+  | T.null instr = Left $ "Could not parse " <> unpack instr
+  | T.length instr == 1 = parseC instr
   | otherwise =
     let h = T.head instr
         address = T.tail instr
@@ -58,18 +60,47 @@ parseA address = (decimal :: Reader Int) address >>= toInstr
       | otherwise = Right $ A' a
 
 parseC :: Text -> Either ParseErrorDescription Instruction
-parseC command = maybe fallBack Right parsed
+parseC instr = maybe fallBack Right $ parse instr
   where
-    parsed = C' <$> a <*> comp <*> dest <*> jump
-    fallBack = Left ("Could not parse computation command " <> unpack command)
+    parse = fromInstrSplit . splitCInstr
+    fallBack = Left ("Could not parse computation command " <> unpack instr)
+
+fromInstrSplit :: CInstrSplit -> Maybe Instruction
+fromInstrSplit (CInstrSplit' dest comp jmp) = C' <$> a <*> comp' <*> dest' <*> jmp'
+  where
     a
-      | command `P.elem` fmap fst computationAZeroLUT = Just 0
-      | command `P.elem` fmap fst computationAOneLUT = Just 1
+      | isCompElem computationAZeroLUT = Just 0
+      | isCompElem computationAOneLUT = Just 1
       | otherwise = Nothing
-    comp = a >>= lookup command . getLUT
-    dest = Just 0
-    jump = Just 0
-    getLUT a' = if a' == 0 then computationAZeroLUT else computationAOneLUT 
+    dest' = join (lookup <$> dest <*> pure destinationLUT) <|> Just 0
+    comp' = a >>= lookup (fromMaybe "" comp) . getCompLUT
+    jmp' = join (lookup <$> jmp <*> pure jumpLUT) <|> Just 0
+    getCompLUT a' = if a' == 0 then computationAZeroLUT else computationAOneLUT
+    isCompElem lut = fromMaybe False $ P.elem <$> comp <*> pure (fmap fst lut)
+
+-- | Split-up text consisting of destination, computation and jump part.
+data CInstrSplit = CInstrSplit'
+  { _destS :: Maybe Text,
+    _compS :: Maybe Text,
+    _jumpS :: Maybe Text
+  }
+  deriving (Show, Eq)
+
+splitCInstr :: Text -> CInstrSplit
+splitCInstr instr =
+  let dest = if hasDest then Just (L.head firstSplit) else Nothing
+      compAndJump = if hasDest then firstSplit !! 1 else L.head firstSplit
+   in splitCompAndJump (CInstrSplit' dest) compAndJump
+  where
+    firstSplit = T.split (== '=') instr
+    hasDest = L.length firstSplit == 2
+
+splitCompAndJump :: (Maybe Text -> Maybe Text -> CInstrSplit) -> Text -> CInstrSplit
+splitCompAndJump f t = f comp jump
+  where
+    comp = if L.length secondSplit <= 2 then Just (L.head secondSplit) else Nothing
+    jump = if L.length secondSplit == 2 then Just (secondSplit !! 1) else Nothing
+    secondSplit = T.split (== ';') t
 
 -- | Lookup table for __A=0__ ('_a') computation command which represents '_comp' of 'Instruction'.
 computationAZeroLUT :: [(Text, Int)]
@@ -93,7 +124,7 @@ computationAZeroLUT =
     ("D&A", 0),
     ("D|A", 21)
   ]
-  
+
 -- | Lookup table for __A=1__ ('_a') computation command which represents '_comp' of 'Instruction'.
 computationAOneLUT :: [(Text, Int)]
 computationAOneLUT =
@@ -112,14 +143,24 @@ computationAOneLUT =
 -- | Lookup table for every destination which represents '_dest' of 'Instruction'.
 destinationLUT :: [(Text, Int)]
 destinationLUT =
-  [ ("", 42),
-    ("M", 1),
-    ("D", 58),
-    ("MD", 12),
-    ("A", 48),
-    ("AM", 13),
-    ("AD", 49),
-    ("AMD", 15)
+  [ ("M", 1),
+    ("D", 2),
+    ("MD", 3),
+    ("A", 4),
+    ("AM", 5),
+    ("AD", 6),
+    ("AMD", 7)
+  ]
+
+jumpLUT :: [(Text, Int)]
+jumpLUT =
+  [ ("JGT", 1),
+    ("JEQ", 2),
+    ("JGE", 3),
+    ("JLT", 4),
+    ("JNE", 5),
+    ("JLE", 6),
+    ("JMP", 7)
   ]
 
 -- | Remove whitespaces, empty lines and comments.
--- test/Hack/AssemblerSpec.hs
+++ test/Hack/AssemblerSpec.hs
@@ -4,7 +4,7 @@ module Hack.AssemblerSpec (spec) where
 
 import Data.Either
 import Data.Text (pack)
-import Hack.Assembler.Internal (Instruction (..), cleanUpCode, isCode, isComment, parseInstruction, Instruction(..))
+import Hack.Assembler.Internal (Instruction (..), cleanUpCode, isCode, isComment, parseInstruction, splitCInstr, CInstrSplit(..), fromInstrSplit)
 import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 import Test.QuickCheck (elements, forAll, listOf1, property)
 import Test.QuickCheck.Instances.Text ()
@@ -19,9 +19,11 @@ spec = do
       show (C' 1 8 6 7) `shouldBe` "C 1 1000 110 111"
       show (C' 0 60 4 5) `shouldBe` "C 0 111100 100 101"
 
-  describe "parseInstruction of A instructions" $ do
+  describe "parseInstruction of general invalid instructions" $ do
     it "handles empty command" $ do
       parseInstruction "" `shouldSatisfy` isLeft
+
+  describe "parseInstruction of A instructions" $ do
     it "parses valid A instruction" $ do
       parseInstruction "@0" `shouldBe` Right (A' 0)
       parseInstruction "@1" `shouldBe` Right (A' 1)
@@ -30,36 +32,125 @@ spec = do
       parseInstruction "@32768" `shouldSatisfy` isLeft
 
   describe "parseInstruction of C instructions" $ do
-    it "parses all possible computations for a=0" $ do
-      parseInstruction "0" `shouldBe` Right (C' 0 42 0 0)
-      parseInstruction "1" `shouldBe` Right (C' 0 63 0 0)
-      parseInstruction "-1" `shouldBe` Right (C' 0 58 0 0)
-      parseInstruction "D" `shouldBe` Right (C' 0 12 0 0)
-      parseInstruction "A" `shouldBe` Right (C' 0 48 0 0)
-      parseInstruction "!D" `shouldBe` Right (C' 0 13 0 0)
-      parseInstruction "!A" `shouldBe` Right (C' 0 49 0 0)
-      parseInstruction "-D" `shouldBe` Right (C' 0 15 0 0)
-      parseInstruction "-A" `shouldBe` Right (C' 0 51 0 0)
-      parseInstruction "D+1" `shouldBe` Right (C' 0 31 0 0)
-      parseInstruction "A+1" `shouldBe` Right (C' 0 55 0 0)
-      parseInstruction "D-1" `shouldBe` Right (C' 0 14 0 0)
-      parseInstruction "A-1" `shouldBe` Right (C' 0 50 0 0)
-      parseInstruction "D+A" `shouldBe` Right (C' 0 2 0 0)
-      parseInstruction "D-A" `shouldBe` Right (C' 0 19 0 0)
-      parseInstruction "A-D" `shouldBe` Right (C' 0 7 0 0)
-      parseInstruction "D&A" `shouldBe` Right (C' 0 0 0 0)
-      parseInstruction "D|A" `shouldBe` Right (C' 0 21 0 0)
-    it "parses all possible computations for a=1" $ do
-      parseInstruction "M" `shouldBe` Right (C' 1 48 0 0)
-      parseInstruction "!M" `shouldBe` Right (C' 1 49 0 0)
-      parseInstruction "-M" `shouldBe` Right (C' 1 51 0 0)
-      parseInstruction "M+1" `shouldBe` Right (C' 1 55 0 0)
-      parseInstruction "M-1" `shouldBe` Right (C' 1 50 0 0)
-      parseInstruction "D+M" `shouldBe` Right (C' 1 2 0 0)
-      parseInstruction "D-M" `shouldBe` Right (C' 1 19 0 0)
-      parseInstruction "M-D" `shouldBe` Right (C' 1 7 0 0)
-      parseInstruction "D&M" `shouldBe` Right (C' 1 0 0 0)
-      parseInstruction "D|M" `shouldBe` Right (C' 1 21 0 0)
+      it "parses all possible computations for a=0" $ do
+        parseInstruction "0" `shouldBe` Right (C' 0 42 0 0)
+        parseInstruction "1" `shouldBe` Right (C' 0 63 0 0)
+        parseInstruction "-1" `shouldBe` Right (C' 0 58 0 0)
+        parseInstruction "D" `shouldBe` Right (C' 0 12 0 0)
+        parseInstruction "A" `shouldBe` Right (C' 0 48 0 0)
+        parseInstruction "!D" `shouldBe` Right (C' 0 13 0 0)
+        parseInstruction "!A" `shouldBe` Right (C' 0 49 0 0)
+        parseInstruction "-D" `shouldBe` Right (C' 0 15 0 0)
+        parseInstruction "-A" `shouldBe` Right (C' 0 51 0 0)
+        parseInstruction "D+1" `shouldBe` Right (C' 0 31 0 0)
+        parseInstruction "A+1" `shouldBe` Right (C' 0 55 0 0)
+        parseInstruction "D-1" `shouldBe` Right (C' 0 14 0 0)
+        parseInstruction "A-1" `shouldBe` Right (C' 0 50 0 0)
+        parseInstruction "D+A" `shouldBe` Right (C' 0 2 0 0)
+        parseInstruction "D-A" `shouldBe` Right (C' 0 19 0 0)
+        parseInstruction "A-D" `shouldBe` Right (C' 0 7 0 0)
+        parseInstruction "D&A" `shouldBe` Right (C' 0 0 0 0)
+        parseInstruction "D|A" `shouldBe` Right (C' 0 21 0 0)
+      it "parses all possible computations for a=1" $ do
+        parseInstruction "M" `shouldBe` Right (C' 1 48 0 0)
+        parseInstruction "!M" `shouldBe` Right (C' 1 49 0 0)
+        parseInstruction "-M" `shouldBe` Right (C' 1 51 0 0)
+        parseInstruction "M+1" `shouldBe` Right (C' 1 55 0 0)
+        parseInstruction "M-1" `shouldBe` Right (C' 1 50 0 0)
+        parseInstruction "D+M" `shouldBe` Right (C' 1 2 0 0)
+        parseInstruction "D-M" `shouldBe` Right (C' 1 19 0 0)
+        parseInstruction "M-D" `shouldBe` Right (C' 1 7 0 0)
+        parseInstruction "D&M" `shouldBe` Right (C' 1 0 0 0)
+        parseInstruction "D|M" `shouldBe` Right (C' 1 21 0 0)
+      it "parses all possible destinations for a=0" $ do
+        parseInstruction "0" `shouldBe` Right (C' 0 42 0 0)
+        parseInstruction "M=0" `shouldBe` Right (C' 0 42 1 0)
+        parseInstruction "D=0" `shouldBe` Right (C' 0 42 2 0)
+        parseInstruction "MD=0" `shouldBe` Right (C' 0 42 3 0)
+        parseInstruction "A=0" `shouldBe` Right (C' 0 42 4 0)
+        parseInstruction "AM=0" `shouldBe` Right (C' 0 42 5 0)
+        parseInstruction "AD=0" `shouldBe` Right (C' 0 42 6 0)
+        parseInstruction "AMD=0" `shouldBe` Right (C' 0 42 7 0)
+      it "parses all possible destinations for a=1" $ do
+        parseInstruction "M=M" `shouldBe` Right (C' 1 48 1 0)
+        parseInstruction "D=!M" `shouldBe` Right (C' 1 49 2 0)
+        parseInstruction "MD=-M" `shouldBe` Right (C' 1 51 3 0)
+        parseInstruction "A=M+1" `shouldBe` Right (C' 1 55 4 0)
+        parseInstruction "AM=M-1" `shouldBe` Right (C' 1 50 5 0)
+        parseInstruction "AD=D+M" `shouldBe` Right (C' 1 2 6 0)
+        parseInstruction "AMD=D-M" `shouldBe` Right (C' 1 19 7 0)
+      it "parses all possible jumps for a=0" $ do
+        parseInstruction "0;JGT" `shouldBe` Right (C' 0 42 0 1)
+        parseInstruction "-1;JEQ" `shouldBe` Right (C' 0 58 0 2)
+        parseInstruction "A;JGE" `shouldBe` Right (C' 0 48 0 3)
+        parseInstruction "!D;JLT" `shouldBe` Right (C' 0 13 0 4)
+        parseInstruction "-D;JNE" `shouldBe` Right (C' 0 15 0 5)
+        parseInstruction "D+1;JLE" `shouldBe` Right (C' 0 31 0 6)
+        parseInstruction "D|A;JMP" `shouldBe` Right (C' 0 21 0 7)
+      it "parses all possible jumps with destination for a=0" $ do
+        parseInstruction "M=0;JGT" `shouldBe` Right (C' 0 42 1 1)
+        parseInstruction "D=-1;JEQ" `shouldBe` Right (C' 0 58 2 2)
+        parseInstruction "MD=A;JGE" `shouldBe` Right (C' 0 48 3 3)
+        parseInstruction "A=!D;JLT" `shouldBe` Right (C' 0 13 4 4)
+        parseInstruction "AM=-D;JNE" `shouldBe` Right (C' 0 15 5 5)
+        parseInstruction "AD=D+1;JLE" `shouldBe` Right (C' 0 31 6 6)
+        parseInstruction "AMD=D|A;JMP" `shouldBe` Right (C' 0 21 7 7)
+
+  describe "splitCInstr" $ do
+    it "gets Computation part" $ do
+      splitCInstr "0" `shouldBe` CInstrSplit' Nothing (Just "0") Nothing
+      splitCInstr "1" `shouldBe` CInstrSplit' Nothing (Just "1") Nothing
+      splitCInstr "-1" `shouldBe` CInstrSplit' Nothing (Just "-1") Nothing
+      splitCInstr "D" `shouldBe` CInstrSplit' Nothing (Just "D") Nothing
+      splitCInstr "A" `shouldBe` CInstrSplit' Nothing (Just "A") Nothing
+      splitCInstr "!D" `shouldBe` CInstrSplit' Nothing (Just "!D") Nothing
+      splitCInstr "!A" `shouldBe` CInstrSplit' Nothing (Just "!A") Nothing
+      splitCInstr "-D" `shouldBe` CInstrSplit' Nothing (Just "-D") Nothing
+      splitCInstr "-A" `shouldBe` CInstrSplit' Nothing (Just "-A") Nothing
+      splitCInstr "D+1" `shouldBe` CInstrSplit' Nothing (Just "D+1") Nothing
+      splitCInstr "A+1" `shouldBe` CInstrSplit' Nothing (Just "A+1") Nothing
+      splitCInstr "D-1" `shouldBe` CInstrSplit' Nothing (Just "D-1") Nothing
+      splitCInstr "A-1" `shouldBe` CInstrSplit' Nothing (Just "A-1") Nothing
+      splitCInstr "D+A" `shouldBe` CInstrSplit' Nothing (Just "D+A") Nothing
+      splitCInstr "D-A" `shouldBe` CInstrSplit' Nothing (Just "D-A") Nothing
+      splitCInstr "A-D" `shouldBe` CInstrSplit' Nothing (Just "A-D") Nothing
+      splitCInstr "D&A" `shouldBe` CInstrSplit' Nothing (Just "D&A") Nothing
+      splitCInstr "D|A" `shouldBe` CInstrSplit' Nothing (Just "D|A") Nothing
+      splitCInstr "M" `shouldBe` CInstrSplit' Nothing (Just "M") Nothing
+      splitCInstr "!M" `shouldBe` CInstrSplit' Nothing (Just "!M") Nothing
+      splitCInstr "-M" `shouldBe` CInstrSplit' Nothing (Just "-M") Nothing
+      splitCInstr "M+1" `shouldBe` CInstrSplit' Nothing (Just "M+1") Nothing
+      splitCInstr "M-1" `shouldBe` CInstrSplit' Nothing (Just "M-1") Nothing
+      splitCInstr "D+M" `shouldBe` CInstrSplit' Nothing (Just "D+M") Nothing
+      splitCInstr "D-M" `shouldBe` CInstrSplit' Nothing (Just "D-M") Nothing
+      splitCInstr "M-D" `shouldBe` CInstrSplit' Nothing (Just "M-D") Nothing
+      splitCInstr "D&M" `shouldBe` CInstrSplit' Nothing (Just "D&M") Nothing
+      splitCInstr "D|M" `shouldBe` CInstrSplit' Nothing (Just "D|M") Nothing
+    it "gets Destination and Computation part" $ do
+      splitCInstr "M=0" `shouldBe` CInstrSplit' (Just "M") (Just "0") Nothing
+      splitCInstr "D=1" `shouldBe` CInstrSplit' (Just "D") (Just "1") Nothing
+      splitCInstr "MD=-1" `shouldBe` CInstrSplit' (Just "MD") (Just "-1") Nothing
+      splitCInstr "A=D" `shouldBe` CInstrSplit' (Just "A") (Just "D") Nothing
+      splitCInstr "AM=A" `shouldBe` CInstrSplit' (Just "AM") (Just "A") Nothing
+      splitCInstr "AD=!D" `shouldBe` CInstrSplit' (Just "AD") (Just "!D") Nothing
+      splitCInstr "AMD=D&A" `shouldBe` CInstrSplit' (Just "AMD") (Just "D&A") Nothing
+      splitCInstr "AMD=D|A" `shouldBe` CInstrSplit' (Just "AMD") (Just "D|A") Nothing
+    it "gets Computation and Jump part" $ do
+      splitCInstr "0;JGT" `shouldBe` CInstrSplit' Nothing (Just "0") (Just "JGT")
+      splitCInstr "1;JEQ" `shouldBe` CInstrSplit' Nothing (Just "1") (Just "JEQ")
+      splitCInstr "-1;JGE" `shouldBe` CInstrSplit' Nothing (Just "-1") (Just "JGE")
+      splitCInstr "D;JLT" `shouldBe` CInstrSplit' Nothing (Just "D") (Just "JLT")
+      splitCInstr "!D;JNE" `shouldBe` CInstrSplit' Nothing (Just "!D") (Just "JNE")
+      splitCInstr "D+1;JLE" `shouldBe` CInstrSplit' Nothing (Just "D+1") (Just "JLE")
+      splitCInstr "D|M;JMP" `shouldBe` CInstrSplit' Nothing (Just "D|M") (Just "JMP")
+    it "gets Destination, Computation and Jump part" $ do
+      splitCInstr "M=0;JGT" `shouldBe` CInstrSplit' (Just "M") (Just "0") (Just "JGT")
+      splitCInstr "D=1;JEQ" `shouldBe` CInstrSplit' (Just "D") (Just "1") (Just "JEQ")
+      splitCInstr "MD=-1;JGE" `shouldBe` CInstrSplit' (Just "MD") (Just "-1") (Just "JGE")
+      splitCInstr "A=D;JLT" `shouldBe` CInstrSplit' (Just "A") (Just "D") (Just "JLT")
+      splitCInstr "AM=A;JNE" `shouldBe` CInstrSplit' (Just "AM") (Just "A") (Just "JNE")
+      splitCInstr "AD=!D;JLE" `shouldBe` CInstrSplit' (Just "AD") (Just "!D") (Just "JLE")
+      splitCInstr "AMD=D&A;JMP" `shouldBe` CInstrSplit' (Just "AMD") (Just "D&A") (Just "JMP")
 
   describe "cleanUpCode" $ do
     it "can handle empty text" $ do
