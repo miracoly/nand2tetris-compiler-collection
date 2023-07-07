7a550b4aee589bb9b7cadb9afd616ea9a0eba038
miracoly <68049792+miracoly@users.noreply.github.com>
2023-07-07 13:03:43 +0200

    compile assembly into machine code

--- test/Hack/AssemblerSpec.hs
+++ test/Hack/AssemblerSpec.hs
@@ -17,6 +17,7 @@ spec = do
   describe "compile" $ do
     it "compiles assembly into machine code" $ do
       compile exampleAssemblyOne `shouldBe` Right exampleCompiledOne
+      compile exampleAssemblyTwo `shouldBe` Right exampleCompiledTwo
   
   describe "Binary is member of Read class" $ do
     it "shows A Instruction" $ do
@@ -595,5 +596,131 @@ exampleCompiledOne =
 0000000000000100
 1110001100000111
 0000000000010010
+1110101010000111
+|]
+
+exampleAssemblyTwo :: Text
+exampleAssemblyTwo =
+  [r|// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+(WAITFORINPUT)
+  @KBD
+  D=M
+  @CHECKWASBLACKBEFORE
+  D;JNE
+
+  @CHECKWASWHITEBEFORE
+  0;JMP
+
+(CHECKWASWHITEBEFORE)
+  @color
+  D=M
+  @WHITE
+  D;JNE
+  @WAITFORINPUT
+  0;JMP
+
+  @WAITFORINPUT
+  D;JMP
+
+(CHECKWASBLACKBEFORE)
+  @color
+  D=M
+  @BLACK
+  D;JEQ
+  @WAITFORINPUT
+  0;JMP
+
+(WHITE)
+  @color
+  M=0
+  @SETSCREEN
+  0;JMP
+
+(BLACK)
+  @color
+  M=-1
+
+(SETSCREEN)
+  @SCREEN
+  D=A
+  @currentscreen
+  M=D
+
+  (SETSCREENPART)
+    @KBD
+    D=A
+    @currentscreen
+    D=D-M
+    @WAITFORINPUT
+    D;JEQ
+    @color
+    D=M
+    @currentscreen
+    A=M
+    M=D
+    @currentscreen
+    M=M+1
+    @SETSCREENPART
+    0;JMP|]
+    
+exampleCompiledTwo :: Text
+exampleCompiledTwo =
+  [r|0110000000000000
+1111110000010000
+0000000000001110
+1110001100000101
+0000000000000110
+1110101010000111
+0000000000010000
+1111110000010000
+0000000000010100
+1110001100000101
+0000000000000000
+1110101010000111
+0000000000000000
+1110001100000111
+0000000000010000
+1111110000010000
+0000000000011000
+1110001100000010
+0000000000000000
+1110101010000111
+0000000000010000
+1110101010001000
+0000000000011010
+1110101010000111
+0000000000010000
+1110111010001000
+0100000000000000
+1110110000010000
+0000000000010001
+1110001100001000
+0110000000000000
+1110110000010000
+0000000000010001
+1111010011010000
+0000000000000000
+1110001100000010
+0000000000010000
+1111110000010000
+0000000000010001
+1111110000100000
+1110001100001000
+0000000000010001
+1111110111001000
+0000000000011110
 1110101010000111
 |]
