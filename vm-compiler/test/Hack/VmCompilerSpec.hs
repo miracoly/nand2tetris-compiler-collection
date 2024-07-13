664c1d7a5ec2ac92e9535397e3def56703608209
miracoly <68049792+miracoly@users.noreply.github.com>
2024-07-13 19:14:31 +0200

    test(vmc): cover parsing of commands

--- vm-compiler/test/Hack/VmCompilerSpec.hs
+++ vm-compiler/test/Hack/VmCompilerSpec.hs
@@ -1,33 +1,54 @@
 module Hack.VmCompilerSpec (spec) where
 
-import Hack.VmCompiler.Internal (pLines, pIndex, pVmSegment, Segment(..))
+import Hack.VmCompiler.Internal
+  ( Segment (..),
+    VmCommand (..),
+    pCommand,
+    pCommands,
+    pSegment,
+  )
 import Test.Hspec (Spec, describe, it, shouldBe)
 import Test.QuickCheck.Instances.Text ()
 import Text.Parsec (parse)
 
 spec :: Spec
 spec = do
-  describe "pVmSegment" $ do
+  describe "pCommands" $ do
+    it "parses multiple arithmetical commands" $ do
+      parse pCommands "" "add\nsub\n" `shouldBe` Right [Add, Sub]
+    it "parses multiple logical commands" $ do
+      parse pCommands "" "eq\ngt\n" `shouldBe` Right [Eq, Gt]
+    it "parses multiple push / pop commands" $ do
+      parse pCommands "" "push constant 7\npop local 0\n"
+        `shouldBe` Right [Push Constant 7, Pop Local 0]
+    it "parses mixed commands" $ do
+      parse pCommands "" "add\npush constant 7\n"
+        `shouldBe` Right [Add, Push Constant 7]
+      parse pCommands "" "eq\npop local 0\n" `shouldBe` Right [Eq, Pop Local 0]
+
+  describe "pCommand" $ do
+    it "parses arithmetical commands" $ do
+      parse pCommand "" "add\n" `shouldBe` Right Add
+      parse pCommand "" "sub\n" `shouldBe` Right Sub
+      parse pCommand "" "neg\n" `shouldBe` Right Neg
+    it "parses logical commands" $ do
+      parse pCommand "" "eq\n" `shouldBe` Right Eq
+      parse pCommand "" "gt\n" `shouldBe` Right Gt
+      parse pCommand "" "lt\n" `shouldBe` Right Lt
+      parse pCommand "" "and\n" `shouldBe` Right And
+      parse pCommand "" "or\n" `shouldBe` Right Or
+      parse pCommand "" "not\n" `shouldBe` Right Not
+    it "parses push / pop commands" $ do
+      parse pCommand "" "push constant 7\n" `shouldBe` Right (Push Constant 7)
+      parse pCommand "" "pop local 0\n" `shouldBe` Right (Pop Local 0)
+
+  describe "pSegment" $ do
     it "parses segment" $ do
-      parse pVmSegment "" "constant " `shouldBe` Right Constant
-      parse pVmSegment "" "local " `shouldBe` Right Local
-      parse pVmSegment "" "argument " `shouldBe` Right Argument
-      parse pVmSegment "" "this " `shouldBe` Right This
-      parse pVmSegment "" "that " `shouldBe` Right That
-      parse pVmSegment "" "temp " `shouldBe` Right Temp
-      parse pVmSegment "" "pointer " `shouldBe` Right Pointer
-      parse pVmSegment "" "static " `shouldBe` Right Static
-
-  describe "pIndex" $ do
-    it "parses index" $ do
-      parse pIndex "" "1\n" `shouldBe` Right 1
-      parse pIndex "" "2\n" `shouldBe` Right 2
-      parse pIndex "" "999\n" `shouldBe` Right 999
-
-  describe "pLines" $ do
-    it "parses one line" $ do
-      parse pLines "" "one line\n" `shouldBe` Right ["one line"]
-    it "parses multiple lines" $ do
-      let text = "line one\nline two\n"
-      let expected = ["line one", "line two"]
-      parse pLines "" text `shouldBe` Right expected
+      parse pSegment "" "constant " `shouldBe` Right Constant
+      parse pSegment "" "local " `shouldBe` Right Local
+      parse pSegment "" "argument " `shouldBe` Right Argument
+      parse pSegment "" "this " `shouldBe` Right This
+      parse pSegment "" "that " `shouldBe` Right That
+      parse pSegment "" "temp " `shouldBe` Right Temp
+      parse pSegment "" "pointer " `shouldBe` Right Pointer
+      parse pSegment "" "static " `shouldBe` Right Static
